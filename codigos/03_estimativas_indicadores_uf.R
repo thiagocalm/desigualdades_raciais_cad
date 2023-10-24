448c7ba7305054e1ad82bf9ecce50783cadce066
Thiago Almeida <64435583+thiagocalm@users.noreply.github.com>
2023-10-24 01:29:48 -0300

    Updating codes for state-level analysis

---
+++ codigos/03_estimativas_indicadores_uf.R
@@ -0,0 +1,116 @@
+#' ------------------------------------------------------
+#' @author Thiago Cordeiro Almeida
+#' @last-update 2023-10-24
+#' @description Estimativas dos indicadores a nível de UF
+#' -----------------------------------------------------
+options(scipen = 9999999)
+rm(list = ls())
+gc()
+
+# bibliotecas -------------------------------------------------------------
+
+if(!require("pacman")) install.packages(("pacman"))
+pacman::p_load(tidyverse, arrow)
+
+# Importacao dos dados ------------------------------------
+
+anos <- 2013:2018
+
+for(i in seq_along(anos)){
+  # importacao de cada ano
+  ano <- anos[i]
+
+  DIR <- paste0("./dados/cad_",ano,"_municipal.parquet")
+
+  # leitura dos dados
+  df <- read_parquet(file = DIR)
+
+  # criando variavel ano
+  df <- df |>
+    mutate(ano = ano)
+
+  # juncao das bases de dados
+  if(i == 1){
+    indicadores_municipais <- df
+  } else{
+    indicadores_municipais <- indicadores_municipais |>
+      bind_rows(df)
+  }
+
+  # proximo loop
+  rm(df)
+  gc()
+  print(paste0("Finalizamos a importacao para o ano: ",ano,"!!!"))
+}
+
+# Calculo dos indicadores -------------------------------------------------
+
+indicadores_uf <- indicadores_municipais |>
+  mutate(
+    uf = substr(cd_ibge,1,2)
+  ) |>
+  group_by(ano, uf) |>
+  reframe(
+    across(c(jovens_15_17_frequentam_em:jovens_brancos_15_29_ocupados,familias_com_jovens_15_29_esgoto:mulheres_brancas_15_19anos_com_filho),
+             ~sum(.x))
+  ) |>
+  left_join(
+    indicadores_municipais |>
+      mutate(
+        uf = substr(cd_ibge,1,2)
+      ) |>
+      group_by(ano, uf) |>
+      reframe(
+        across(-c(jovens_15_17_frequentam_em:jovens_brancos_15_29_ocupados,familias_com_jovens_15_29_esgoto:mulheres_brancas_15_19anos_com_filho),
+               ~mean(.x, na.rm = TRUE))
+      ),
+    by = c("ano","uf"),
+    keep = FALSE
+  ) |>
+  group_by(ano, uf) |>
+  reframe(
+    # Ensino Medio
+    indicador_em = round(100*(jovens_15_17_frequentam_em/jovens_15_17),2),
+    indicador_em_negros = round(100*(jovens_negros_15_17_frequentam_em/jovens_negros_15_17),2),
+    indicador_em_brancos = round(100*(jovens_brancos_15_17_frequentam_em/jovens_brancos_15_17),2),
+
+    # infraestrutura
+    indicador_acesso_esgoto = round(100*(familias_com_jovens_15_29_esgoto/familias_com_jovens_15_29),2),
+    indicador_acesso_esgoto_negros = round(100*(familias_com_jovens_negros_15_29_esgoto/familias_com_jovens_negros_15_29),2),
+    indicador_acesso_esgoto_brancos = round(100*(familias_com_jovens_brancos_15_29_esgoto/familias_com_jovens_brancos_15_29),2),
+
+    # PBF
+    indicador_pbf = round(100*(familias_com_jovens_15_29_pbf/familias_com_jovens_15_29),2),
+    indicador_pbf_negros = round(100*(familias_com_jovens_negros_15_29_pbf/familias_com_jovens_negros_15_29),2),
+    indicador_pbf_brancos = round(100*(familias_com_jovens_brancos_15_29_pbf/familias_com_jovens_brancos_15_29),2),
+
+    # renda de outras fontes
+    indicador_renda_outras_fontes = round(100*indicador_prop_renda_outras_fontes,2),
+    indicador_renda_outras_fontes_negros = round(100*indicador_prop_renda_outras_fontes_negros,2),
+    indicador_renda_outras_fontes_brancos = round(100*indicador_prop_renda_outras_fontes_branco,2),
+
+    # Maes adolescentes
+    indicador_maes_adolescentes = round(100*(mulheres_15_19anos_com_filho/mulheres_15_19anos),2),
+    indicador_maes_adolescentes_negras = round(100*(mulheres_negras_15_19anos_com_filho/mulheres_negras_15_19anos),2),
+    indicador_maes_adolescentes_brancas = round(100*(mulheres_brancas_15_19anos_com_filho/mulheres_brancas_15_19anos),2),
+
+    # Desocupação ou inatividade
+    indicador_desocupados = round(100*(jovens_15_29_desocupados/jovens_15_29),2),
+    indicador_desocupados_negros = round(100*(jovens_negros_15_29_desocupados/jovens_negros_15_29),2),
+    indicador_desocupados_brancos = round(100*(jovens_brancos_15_29_desocupados/jovens_brancos_15_29),2),
+
+    # Informalidade
+    indicador_informalidade = round(100*(jovens_15_29_ocupados_informalidade/jovens_15_29_ocupados),2),
+    indicador_informalidade_negros = round(100*(jovens_negros_15_29_ocupados_informalidade/jovens_negros_15_29_ocupados),2),
+    indicador_informalidade_brancos = round(100*(jovens_brancos_15_29_ocupados_informalidade/jovens_brancos_15_29_ocupados),2)
+  ) |>
+  mutate(
+    regiao = substr(uf,1,1)
+  ) |>
+  select(ano,regiao, uf, everything()) |>
+  mutate_all(~ replace_na(.x,0))
+
+# Exportacao dos dados ----------------------------------------------------
+
+write_parquet(indicadores_uf, sink = paste0("./output/base de dados - desigualdade/cad_indicadores_uf.parquet"))
+write_csv2(indicadores_uf, file = paste0("./output/base de dados - desigualdade/cad_indicadores_uf.csv"))
--- codigos/06_analise_exploratoria_violencia_desigualdade.R
+++ codigos/06_analise_exploratoria_violencia_desigualdade.R
@@ -1,6 +1,6 @@
 #' ------------------------------------------------------
 #' @author Thiago Cordeiro Almeida
-#' @last-update 2023-10-20
+#' @last-update 2023-10-24
 #' @description Análises exploratórias dos dados completos
 #' -----------------------------------------------------
 options(scipen = 9999999)
@@ -17,12 +17,25 @@ DIR <- "./output/base de dados - violencia e desigualdade"
 
 # leitura dos dados
 df <- read_parquet(file = file.path(DIR, "base_violencia_desigualdade.parquet"))
+df_uf <- read_parquet(file = "./output/base de dados - desigualdade/cad_indicadores_uf.parquet")
 
 # Definicao da base de trabalho -------------------------------------------
 
+cods_uf <- tibble(cod = c("11","12","13","14","15","16","17","21","22","23","24","25","26","27",
+                          "28","29","31","32","33","35","41","42","43","50","51","52","53"),
+                  names = c("Rondônia","Acre","Amazonas", "Roraima","Pará","Amapá","Tocantins","Maranhão",
+                            "Piauí", "Ceará","Rio Grande do Norte","Paraíba","Pernambuco","Alagoas","Sergipe",
+                            "Bahia","Minas Gerais","Espírito Santo","Rio de Janeiro","São Paulo","Paraná","Santa Catarina",
+                            "Rio Grande do Sul","Mato Grosso do Sul","Mato Grosso","Goiás","Distrito Federal"))
+
 # Nota: vamos trabalhar somente com os municipios com populacao acima de 5000 habitantes
 df_analises <- df |>
-  filter(pop_faixas >= 2)
+  filter(pop_faixas >= 2) |>
+  mutate(uf = factor(uf, levels = cods_uf$cod, labels = cods_uf$names))
+
+df_uf <- df_uf |>
+  mutate(uf = factor(uf, levels = cods_uf$cod, labels = cods_uf$names))
+
 
 # Exploratoria de desigualdade ------------------------------------------------------------
 
@@ -174,7 +187,21 @@ t1_br <- df_analises |>
 
 # Por UF
 
-t1_uf <- df_analises |>
+t1_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_em_negros/indicador_em_brancos) |>
+  select(ano, uf, indicador_em_negros, indicador_em_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano,uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_em_negros, indicador_em_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+# Por UF e munic
+
+t1_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_em_negros/indicador_em_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_em_negros, indicador_em_brancos, razao) |>
@@ -205,7 +232,21 @@ t2_br <- df_analises |>
 
 # Por UF
 
-t2_uf <- df_analises |>
+t2_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_acesso_esgoto_negros/indicador_acesso_esgoto_brancos) |>
+  select(ano, uf, indicador_acesso_esgoto_negros,indicador_acesso_esgoto_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_acesso_esgoto_negros,indicador_acesso_esgoto_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+# Por UF e munic
+
+t2_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_acesso_esgoto_negros/indicador_acesso_esgoto_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_acesso_esgoto_negros,indicador_acesso_esgoto_brancos, razao) |>
@@ -236,7 +277,21 @@ t3_br <- df_analises |>
 
 # Por UF
 
-t3_uf <- df_analises |>
+t3_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_pbf_negros/indicador_pbf_brancos) |>
+  select(ano, uf, indicador_pbf_negros,indicador_pbf_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_pbf_negros,indicador_pbf_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+# Por UF e munic
+
+t3_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_pbf_negros/indicador_pbf_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_pbf_negros,indicador_pbf_brancos, razao) |>
@@ -267,7 +322,21 @@ t4_br <- df_analises |>
 
 # Por UF
 
-t4_uf <- df_analises |>
+t4_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_renda_outras_fontes_negros/indicador_renda_outras_fontes_brancos) |>
+  select(ano, uf, indicador_renda_outras_fontes_negros,indicador_renda_outras_fontes_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_renda_outras_fontes_negros,indicador_renda_outras_fontes_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+# Por UF e munic
+
+t4_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_renda_outras_fontes_negros/indicador_renda_outras_fontes_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_renda_outras_fontes_negros,indicador_renda_outras_fontes_brancos, razao) |>
@@ -298,7 +367,22 @@ t5_br <- df_analises |>
 
 # Por UF
 
-t5_uf <- df_analises |>
+t5_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_maes_adolescentes_negras/indicador_maes_adolescentes_brancas) |>
+  select(ano, uf, indicador_maes_adolescentes_negras,indicador_maes_adolescentes_brancas, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_maes_adolescentes_negras,indicador_maes_adolescentes_brancas, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+
+# Por UF e munic
+
+t5_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_maes_adolescentes_negras/indicador_maes_adolescentes_brancas) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_maes_adolescentes_negras,indicador_maes_adolescentes_brancas, razao) |>
@@ -329,7 +413,22 @@ t6_br <- df_analises |>
 
 # Por UF
 
-t6_uf <- df_analises |>
+t6_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_desocupados_negros/indicador_desocupados_brancos) |>
+  select(ano, uf, indicador_desocupados_negros,indicador_desocupados_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_desocupados_negros,indicador_desocupados_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+
+# Por UF e munic
+
+t6_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_desocupados_negros/indicador_desocupados_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_desocupados_negros,indicador_desocupados_brancos, razao) |>
@@ -360,7 +459,21 @@ t7_br <- df_analises |>
 
 # Por UF
 
-t7_uf <- df_analises |>
+t7_uf <- df_uf |>
+  filter(ano %in% 2013:2018) |>
+  mutate(razao = indicador_informalidade_negros/indicador_informalidade_brancos) |>
+  select(ano, uf, indicador_informalidade_negros,indicador_informalidade_brancos, razao) |>
+  filter(!is.infinite(razao)) |>
+  arrange(desc(razao)) |>
+  group_by(ano, uf) |>
+  slice(1:10) |>
+  pivot_wider(names_from = ano, values_from = c(indicador_informalidade_negros,indicador_informalidade_brancos, razao)) |>
+  select(uf, ends_with("_2013"),ends_with("_2014"),
+         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+
+# Por UF e munic
+
+t7_uf_munic <- df_analises |>
   filter(ano %in% 2013:2018) |>
   mutate(razao = indicador_informalidade_negros/indicador_informalidade_brancos) |>
   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_informalidade_negros,indicador_informalidade_brancos, razao) |>
@@ -373,66 +486,66 @@ t7_uf <- df_analises |>
          ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
 
 #'--------------------------------------------------------------------
-# 8 - Homicidios
-
-# Brasil como um todo
-
-t8_br <- df_analises |>
-  filter(ano %in% 2013:2021) |>
-  mutate(razao = indicador_homicidios_negros/indicador_homicidios_brancos) |>
-  select(ano, cd_municipio_6digitos, nome_municipio, indicador_homicidios_negros,indicador_homicidios_brancos, razao) |>
-  filter(!is.infinite(razao)) |>
-  arrange(desc(razao)) |>
-  group_by(ano) |>
-  slice(1:10) |>
-  pivot_wider(names_from = ano, values_from = c(indicador_homicidios_negros,indicador_homicidios_brancos, razao)) |>
-  select(cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
-         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
-
-# Por UF
-
-t8_uf <- df_analises |>
-  filter(ano %in% 2013:2021) |>
-  mutate(razao = indicador_homicidios_negros/indicador_homicidios_brancos) |>
-  select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_homicidios_negros,indicador_homicidios_brancos, razao) |>
-  filter(!is.infinite(razao)) |>
-  arrange(desc(razao)) |>
-  group_by(ano, uf) |>
-  slice(1:10) |>
-  pivot_wider(names_from = ano, values_from = c(indicador_homicidios_negros,indicador_homicidios_brancos, razao)) |>
-  select(uf, cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
-         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
-
-#'--------------------------------------------------------------------
-# 9 - Intervenção legal
-
-# Brasil como um todo
-
-t9_br <- df_analises |>
-  filter(ano %in% 2013:2021) |>
-  mutate(razao = indicador_intervencao_negros/indicador_intervencao_brancos) |>
-  select(ano, cd_municipio_6digitos, nome_municipio, indicador_intervencao_negros,indicador_intervencao_brancos, razao) |>
-  filter(!is.infinite(razao)) |>
-  arrange(desc(razao)) |>
-  group_by(ano) |>
-  slice(1:10) |>
-  pivot_wider(names_from = ano, values_from = c(indicador_intervencao_negros,indicador_intervencao_brancos, razao)) |>
-  select(cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
-         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
-
-# Por UF
-
-t9_uf <- df_analises |>
-  filter(ano %in% 2013:2021) |>
-  mutate(razao = indicador_intervencao_negros/indicador_intervencao_brancos) |>
-  select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_intervencao_negros,indicador_intervencao_brancos, razao) |>
-  filter(!is.infinite(razao)) |>
-  arrange(desc(razao)) |>
-  group_by(ano, uf) |>
-  slice(1:10) |>
-  pivot_wider(names_from = ano, values_from = c(indicador_intervencao_negros,indicador_intervencao_brancos, razao)) |>
-  select(uf, cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
-         ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+#' # 8 - Homicidios
+#'
+#' # Brasil como um todo
+#'
+#' t8_br <- df_analises |>
+#'   filter(ano %in% 2013:2021) |>
+#'   mutate(razao = indicador_homicidios_negros/indicador_homicidios_brancos) |>
+#'   select(ano, cd_municipio_6digitos, nome_municipio, indicador_homicidios_negros,indicador_homicidios_brancos, razao) |>
+#'   filter(!is.infinite(razao)) |>
+#'   arrange(desc(razao)) |>
+#'   group_by(ano) |>
+#'   slice(1:10) |>
+#'   pivot_wider(names_from = ano, values_from = c(indicador_homicidios_negros,indicador_homicidios_brancos, razao)) |>
+#'   select(cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
+#'          ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+#'
+#' # Por UF e munic
+#'
+#' t8_uf_munic <- df_analises |>
+#'   filter(ano %in% 2013:2021) |>
+#'   mutate(razao = indicador_homicidios_negros/indicador_homicidios_brancos) |>
+#'   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_homicidios_negros,indicador_homicidios_brancos, razao) |>
+#'   filter(!is.infinite(razao)) |>
+#'   arrange(desc(razao)) |>
+#'   group_by(ano, uf) |>
+#'   slice(1:10) |>
+#'   pivot_wider(names_from = ano, values_from = c(indicador_homicidios_negros,indicador_homicidios_brancos, razao)) |>
+#'   select(uf, cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
+#'          ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+#'
+#' #'--------------------------------------------------------------------
+#' # 9 - Intervenção legal
+#'
+#' # Brasil como um todo
+#'
+#' t9_br <- df_analises |>
+#'   filter(ano %in% 2013:2021) |>
+#'   mutate(razao = indicador_intervencao_negros/indicador_intervencao_brancos) |>
+#'   select(ano, cd_municipio_6digitos, nome_municipio, indicador_intervencao_negros,indicador_intervencao_brancos, razao) |>
+#'   filter(!is.infinite(razao)) |>
+#'   arrange(desc(razao)) |>
+#'   group_by(ano) |>
+#'   slice(1:10) |>
+#'   pivot_wider(names_from = ano, values_from = c(indicador_intervencao_negros,indicador_intervencao_brancos, razao)) |>
+#'   select(cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
+#'          ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
+#'
+#' # Por UF e munic
+#'
+#' t9_uf_munic <- df_analises |>
+#'   filter(ano %in% 2013:2021) |>
+#'   mutate(razao = indicador_intervencao_negros/indicador_intervencao_brancos) |>
+#'   select(ano, uf, cd_municipio_6digitos, nome_municipio, indicador_intervencao_negros,indicador_intervencao_brancos, razao) |>
+#'   filter(!is.infinite(razao)) |>
+#'   arrange(desc(razao)) |>
+#'   group_by(ano, uf) |>
+#'   slice(1:10) |>
+#'   pivot_wider(names_from = ano, values_from = c(indicador_intervencao_negros,indicador_intervencao_brancos, razao)) |>
+#'   select(uf, cd_municipio_6digitos, nome_municipio, ends_with("_2013"),ends_with("_2014"),
+#'          ends_with("_2015"),ends_with("_2016"),ends_with("_2017"),ends_with("_2018"))
 
 
 # Exportacao dos dados ----------------------------------------------------
@@ -443,43 +556,58 @@ wb <- createWorkbook()
 
 addWorksheet(wb, "Ensino_Medio")
 addWorksheet(wb, "Ensino_Medio_UF")
+addWorksheet(wb, "Ensino_Medio_UF_munic")
 addWorksheet(wb, "Acess_Esgoto")
 addWorksheet(wb, "Acess_Esgoto_UF")
+addWorksheet(wb, "Acess_Esgoto_UF_munic")
 addWorksheet(wb, "PBF")
 addWorksheet(wb, "PBF_UF")
+addWorksheet(wb, "PBF_UF_munic")
 addWorksheet(wb, "Renda_outras")
 addWorksheet(wb, "Renda_outras_UF")
+addWorksheet(wb, "Renda_outras_UF_munic")
 addWorksheet(wb, "Maes_adolesc")
 addWorksheet(wb, "Maes_adolesc_UF")
+addWorksheet(wb, "Maes_adolesc_UF_munic")
 addWorksheet(wb, "Desocupados")
 addWorksheet(wb, "Desocupados_UF")
+addWorksheet(wb, "Desocupados_UF_munic")
 addWorksheet(wb, "Informalidade")
 addWorksheet(wb, "Informalidade_UF")
-addWorksheet(wb, "Homicidios")
-addWorksheet(wb, "Homicidios_UF")
-addWorksheet(wb, "Intervencoes")
-addWorksheet(wb, "Intervencoes_UF")
+addWorksheet(wb, "Informalidade_UF_munic")
+# addWorksheet(wb, "Homicidios")
+# addWorksheet(wb, "Homicidios_UF_munic")
+# addWorksheet(wb, "Intervencoes")
+# addWorksheet(wb, "Intervencoes_UF_munic")
 
 # inserir dados nas planilhas
 writeData(wb, 1, t1_br)
 writeData(wb, 2, t1_uf)
+writeData(wb, 2, t1_uf_munic)
 writeData(wb, 3, t2_br)
 writeData(wb, 4, t2_uf)
+writeData(wb, 4, t2_uf_munic)
 writeData(wb, 5, t3_br)
 writeData(wb, 6, t3_uf)
+writeData(wb, 6, t3_uf_munic)
 writeData(wb, 7, t4_br)
 writeData(wb, 8, t4_uf)
+writeData(wb, 8, t4_uf_munic)
 writeData(wb, 9, t5_br)
 writeData(wb, 10, t5_uf)
+writeData(wb, 10, t5_uf_munic)
 writeData(wb, 11, t6_br)
 writeData(wb, 12, t6_uf)
+writeData(wb, 12, t6_uf_munic)
 writeData(wb, 13, t7_br)
 writeData(wb, 14, t7_uf)
-writeData(wb, 15, t8_br)
-writeData(wb, 16, t8_uf)
-writeData(wb, 17, t9_br)
-writeData(wb, 18, t9_uf)
+writeData(wb, 14, t7_uf_munic)
+# writeData(wb, 15, t8_br)
+# writeData(wb, 16, t8_uf_munic)
+# writeData(wb, 17, t9_br)
+# writeData(wb, 18, t9_uf_munic)
 
 # exportar xlsx
 
 saveWorkbook(wb, file = file.path(DIR, paste0("resultados - ranking.xlsx")),overwrite = TRUE)
+
