ceff772d41ee4d02c4955750728498e07cf1881f
thiagocalm <thiagocalm@outlook.com>
2023-12-07 19:15:07 -0300

    Inserting legal interventions analysis

--- codigos/04_analise_exploratoria_violencia.R
+++ codigos/04_analise_exploratoria_violencia.R
@@ -10,7 +10,7 @@ gc()
 # bibliotecas -------------------------------------------------------------
 
 if(!require("pacman")) install.packages(("pacman"))
-pacman::p_load(tidyverse, arrow, haven)
+pacman::p_load(tidyverse, arrow, haven, grid)
 
 # Importacao dos dados ----------------------------------------------------
 
@@ -19,8 +19,7 @@ df_homicidios <- read_sav(
   file = file.path("./output","base de dados - violencia","Homicidios.sav")
 )
 
-
-# Descritivas - Numeros absolutos -----------------------------------------
+# Descritivas - homicidios -----------------------------------------
 
 # Número de homicídios registrados
 
@@ -62,32 +61,32 @@ df_homicidios %>%
   reframe(absoluto = n(), .by = c(Ano, sexo)) %>%
   mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(Ano))
 
-# Distribuição dos homicidios etária dos homicídios - POR SEXO E RAÇA
+# Distribuição etária dos homicídios - POR SEXO E RAÇA
 
 df_homicidios %>%
   mutate(
     sexo2 = case_when(sexo == "1" ~ "Masculino", sexo == "2" ~ "Feminino", TRUE ~ "9"),
     raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
-    idade = case_when(
-      idade2 %in% 0:4 ~ 1,
-      idade2 %in% 5:9 ~ 2,
-      idade2 %in% 10:14 ~ 3,
-      idade2 %in% 15:19 ~ 4,
-      idade2 %in% 20:24 ~ 5,
-      idade2 %in% 25:29 ~ 6,
-      idade2 %in% 30:34 ~ 7,
-      idade2 %in% 35:39 ~ 8,
-      idade2 %in% 40:44 ~ 9,
-      idade2 %in% 45:49 ~ 10,
-      idade2 %in% 50:54 ~ 11,
-      idade2 %in% 55:59 ~ 12,
-      idade2 %in% 60:64 ~ 13,
-      idade2 %in% 65:69 ~ 14,
-      idade2 %in% 70:74 ~ 15,
-      idade2 %in% 75:79 ~ 16,
-      is.na(idade2) ~ NA_real_,
-      TRUE ~ 17
-    )
+    idade = as.factor(case_when(
+      idade2 %in% 0:4 ~ "0-4",
+      idade2 %in% 5:9 ~ "5-9",
+      idade2 %in% 10:14 ~ "10-14",
+      idade2 %in% 15:19 ~ "15-19",
+      idade2 %in% 20:24 ~ "20-24",
+      idade2 %in% 25:29 ~ "25-29",
+      idade2 %in% 30:34 ~ "30-34",
+      idade2 %in% 35:39 ~ "35-39",
+      idade2 %in% 40:44 ~ "40-44",
+      idade2 %in% 45:49 ~ "45-49",
+      idade2 %in% 50:54 ~ "50-54",
+      idade2 %in% 55:59 ~ "55-59",
+      idade2 %in% 60:64 ~ "60-64",
+      idade2 %in% 65:69 ~ "65-69",
+      idade2 %in% 70:74 ~ "70-74",
+      idade2 %in% 75:79 ~ "75-79",
+      is.na(idade2) ~ NA_character_,
+      TRUE ~ "80+"
+    ))
   ) %>%
   filter(sexo2 != "9") %>%
   filter(raca != "9") %>%
@@ -95,56 +94,232 @@ df_homicidios %>%
   mutate(
     idade = factor(
       idade,
-      levels = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17),
-      labels = seq(0,80,5)
-    )
-  ) %>%
+      levels = c(
+        "0-4","5-9","10-14",
+        "15-19","20-24","25-29",
+        "30-34","35-39","40-44",
+        "45-49","50-54","55-59",
+        "60-64","65-69","70-74",
+        "75-79","80+")
+    )) %>%
   reframe(absoluto = n(), .by = c(sexo2, raca, idade)) %>%
   mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(sexo2, raca)) %>%
-  bind_rows(
-    df_homicidios %>%
-      mutate(
-        sexo2 = case_when(sexo == "1" ~ "Masculino", sexo == "2" ~ "Feminino", TRUE ~ "9"),
-        raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
-        idade = case_when(
-          idade2 %in% 0:4 ~ 1,
-          idade2 %in% 5:9 ~ 2,
-          idade2 %in% 10:14 ~ 3,
-          idade2 %in% 15:19 ~ 4,
-          idade2 %in% 20:24 ~ 5,
-          idade2 %in% 25:29 ~ 6,
-          idade2 %in% 30:34 ~ 7,
-          idade2 %in% 35:39 ~ 8,
-          idade2 %in% 40:44 ~ 9,
-          idade2 %in% 45:49 ~ 10,
-          idade2 %in% 50:54 ~ 11,
-          idade2 %in% 55:59 ~ 12,
-          idade2 %in% 60:64 ~ 13,
-          idade2 %in% 65:69 ~ 14,
-          idade2 %in% 70:74 ~ 15,
-          idade2 %in% 75:79 ~ 16,
-          is.na(idade2) ~ NA_real_,
-          TRUE ~ 17
-        )
-      ) %>%
-      filter(sexo2 != "9") %>%
-      filter(!is.na(idade)) %>%
-      mutate(
-        idade = factor(
-          idade,
-          levels = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17),
-          labels = seq(0,80,5)
-          )
-      ) %>%
-      reframe(absoluto = n(), .by = c(sexo2, idade)) %>%
-      mutate(raca = "Total",relativo = 100*(absoluto/sum(absoluto)), .by = c(sexo2)) %>%
-      select(sexo2, raca, idade, absoluto, relativo)
-  ) %>%
   arrange(sexo2, raca,idade) %>%
   ggplot() +
-  aes(x = ifelse(sexo2 == "Masculino", -relativo,relativo), y = idade, group = raca) +
-  geom_line(aes(color = raca), linewidth = 1.05) +
-  theme_light()
+  aes(x = ifelse(sexo2 == "Masculino", -relativo,relativo), y = idade) +
+  geom_col(aes(fill = raca), position = position_dodge()) +
+  geom_vline(xintercept = 0, linewidth = .8, color = "black") +
+  scale_fill_brewer(palette = "Accent") +
+  annotate("text", x=-15, y="60-64", label= "Masculino",color = "grey") +
+  annotate("text", x=10, y="60-64", label= "Feminino",color = "grey") +
+  theme_light() +
+  labs(
+    y = "Grupos etários (quinquenais)"
+  ) +
+  theme(
+    legend.title = element_blank(),
+    axis.title.y = element_text(face = "bold", size = 14),
+    axis.text.y = element_text(size = 12),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank()
+  )
+
+# Distribuição dos homicídios entre jovens e restante - GERAL
+
+df_homicidios %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  reframe(absoluto = n(), .by = c(jovem)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Distribuição dos homicídios entre jovens e restante - POR RAÇA
+
+df_homicidios %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  filter(raca != "9") %>%
+  reframe(absoluto = n(), .by = c(raca,jovem)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(raca))
+
+# Distribuição dos homicídios entre jovens e restante - POR RAÇA
+
+df_homicidios %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  filter(raca != "9") %>%
+  reframe(absoluto = n(), .by = c(raca,jovem, Ano)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(Ano, raca)) %>%
+  arrange(raca, jovem, Ano) %>%
+  filter(jovem == "Jovem") %>%
+  print(n = 20)
+
+# Descritivas - intervencioes -----------------------------------------
+
+# Número de homicídios registrados
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  summarise(absoluto = n(), .by = homicidio)
+
+# Número de homicídios registrados - POR ANO
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  reframe(absoluto = n(), .by = Ano) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Número de homicídios registrados - POR TIPO
+
+df_homicidios %>%
+  reframe(absoluto = n(), .by = InterLegal) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Número de homicídios com RAÇA não registrada
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(raca_registrada = case_when(racacor2 %in% 1:8 ~ 1, TRUE ~ 0)) %>%
+  reframe(absoluto = n(), .by = raca_registrada) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Número de homicídios com RAÇA não registrada - POR ANO
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(raca_registrada = case_when(racacor2 %in% 1:8 ~ 1, TRUE ~ 0)) %>%
+  reframe(absoluto = n(), .by = c(raca_registrada, Ano)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(Ano))
+
+# Distribuição dos homicidios por sexo
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(sexo = case_when(sexo %in% c("0","9") ~ "9", TRUE ~ sexo)) %>%
+  reframe(absoluto = n(), .by = c(sexo)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Distribuição dos homicidios por sexo - POR ANO
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(sexo = case_when(sexo %in% c("0","9") ~ "9", TRUE ~ sexo)) %>%
+  filter(sexo != "9") %>%
+  reframe(absoluto = n(), .by = c(Ano, sexo)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(Ano))
+
+# Distribuição etária dos homicídios - POR SEXO E RAÇA
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(
+    sexo2 = case_when(sexo == "1" ~ "Masculino", sexo == "2" ~ "Feminino", TRUE ~ "9"),
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    idade = as.factor(case_when(
+      idade2 %in% 0:4 ~ "0-4",
+      idade2 %in% 5:9 ~ "5-9",
+      idade2 %in% 10:14 ~ "10-14",
+      idade2 %in% 15:19 ~ "15-19",
+      idade2 %in% 20:24 ~ "20-24",
+      idade2 %in% 25:29 ~ "25-29",
+      idade2 %in% 30:34 ~ "30-34",
+      idade2 %in% 35:39 ~ "35-39",
+      idade2 %in% 40:44 ~ "40-44",
+      idade2 %in% 45:49 ~ "45-49",
+      idade2 %in% 50:54 ~ "50-54",
+      idade2 %in% 55:59 ~ "55-59",
+      idade2 %in% 60:64 ~ "60-64",
+      idade2 %in% 65:69 ~ "65-69",
+      idade2 %in% 70:74 ~ "70-74",
+      idade2 %in% 75:79 ~ "75-79",
+      is.na(idade2) ~ NA_character_,
+      TRUE ~ "80+"
+    ))
+  ) %>%
+  filter(sexo2 != "9") %>%
+  filter(raca != "9") %>%
+  filter(!is.na(idade)) %>%
+  mutate(
+    idade = factor(
+      idade,
+      levels = c(
+        "0-4","5-9","10-14",
+        "15-19","20-24","25-29",
+        "30-34","35-39","40-44",
+        "45-49","50-54","55-59",
+        "60-64","65-69","70-74",
+        "75-79","80+")
+    )) %>%
+  reframe(absoluto = n(), .by = c(sexo2, raca, idade)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(sexo2, raca)) %>%
+  arrange(sexo2, raca,idade) %>%
+  ggplot() +
+  aes(x = ifelse(sexo2 == "Masculino", -relativo,relativo), y = idade) +
+  geom_col(aes(fill = raca), position = position_dodge()) +
+  geom_vline(xintercept = 0, linewidth = .8, color = "black") +
+  scale_fill_brewer(palette = "Accent") +
+  annotate("text", x=-15, y="60-64", label= "Masculino",color = "grey") +
+  annotate("text", x=10, y="60-64", label= "Feminino",color = "grey") +
+  theme_light() +
+  labs(
+    y = "Grupos etários (quinquenais)"
+  ) +
+  theme(
+    legend.title = element_blank(),
+    axis.title.y = element_text(face = "bold", size = 14),
+    axis.text.y = element_text(size = 12),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank()
+  )
+
+# Distribuição dos homicídios entre jovens e restante - GERAL
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  reframe(absoluto = n(), .by = c(jovem)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)))
+
+# Distribuição dos homicídios entre jovens e restante - POR RAÇA
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  filter(raca != "9") %>%
+  reframe(absoluto = n(), .by = c(raca,jovem)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(raca))
+
+# Distribuição dos homicídios entre jovens e restante - POR RAÇA
+
+df_homicidios %>%
+  filter(InterLegal == 1) %>%
+  mutate(
+    raca = case_when(racacor2 == 1 ~ "Branca", racacor2 == 2 ~ "Negra", TRUE ~ "9"),
+    jovem = case_when(idade2 %in% 15:29 ~ "Jovem", is.na(idade2) ~ NA_character_, TRUE ~ "Restante")
+  ) %>%
+  filter(!is.na(jovem)) %>%
+  filter(raca != "9") %>%
+  reframe(absoluto = n(), .by = c(raca,jovem, Ano)) %>%
+  mutate(relativo = 100*(absoluto/sum(absoluto)), .by = c(Ano, raca)) %>%
+  arrange(raca, jovem, Ano) %>%
+  filter(jovem == "Jovem") %>%
+  print(n = 20)
 
 # Exploratória da distribuição --------------------------------------------
 
